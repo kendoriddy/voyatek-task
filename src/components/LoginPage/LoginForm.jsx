bfd29cde3e01c60cc553adc20f04155f23c7b378
Kehinde Onifade <onifkay@gmail.com>
2024-01-27 03:38:31 +0100

    added tailwindcss for styling

--- src/components/LoginPage/LoginForm.jsx
+++ src/components/LoginPage/LoginForm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
 export default LoginForm;
