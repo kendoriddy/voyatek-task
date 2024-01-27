f0f9f5257c0059245c32cf7f4d6d10581e567600
Kehinde Onifade <onifkay@gmail.com>
2024-01-27 05:13:44 +0100

    login done

--- src/components/LoginPage/LoginForm.jsx
+++ src/components/LoginPage/LoginForm.jsx
@@ -1,20 +1,57 @@
-import React from "react";
-import "../../assets/styles/LoginForm.css";
+import React, { useState } from "react";
+import { TextField, InputAdornment, IconButton, Box } from "@mui/material";
+import { Link, Visibility, VisibilityOff } from "@mui/icons-material";
+import visibility from "../../assets/images/svg/visibility_on.svg";
+import visibilityOff from "../../assets/images/svg/visibility_off.svg";
 
 const LoginForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <div className="form-container">
-      <h1 className="text-3xl font-bold underline">Login</h1>
+    <div className="w-full">
       <form>
-        <div className="form-field">
-          <input type="email" required autoComplete="new-password" />
-          <label>Business Email</label>
+        <div className="flex flex-col gap-6">
+          <TextField id="outlined-basic" label="Business Email" variant="outlined" fullWidth />
+          <div className="flex flex-col gap-2">
+            <TextField
+              id="outlined-password-input"
+              label="Enter your password"
+              type={showPassword ? "text" : "password"}
+              variant="outlined"
+              fullWidth
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                    >
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <span className="flex justify-end text-[#025C56] text-xs font-medium px-[14px]">
+              Forgot password?
+            </span>
+          </div>
         </div>
-        <div className="form-field">
-          <input type="password" required autoComplete="new-password" />
-          <label>Password</label>
+        <div className="flex flex-col gap-4">
+          <button
+            type="submit"
+            className="text-center w-full bg-[#025C56] mt-10 px-6 py-3 rounded-[100px] text-white"
+          >
+            Sign in
+          </button>
+          <p className="text-[#025C56] font-medium text-xs text-center">
+            Don’t have an account on Project-X? Sign up
+          </p>
         </div>
-        <button type="submit">Submit</button>
       </form>
     </div>
   );
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,7 +5,7 @@ import Welcome from "../components/LoginPage/Welcome";
 const LoginPage = () => {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <div className="m-auto w-[80vw] md:w-[50vw] flex flex-col p-10 gap-10 items-center justify-center bg-[#fff] rounded-2xl">
+      <div className="m-auto w-[80vw] md:w-[35vw] flex flex-col p-10 gap-10 items-center justify-center bg-[#fff] rounded-2xl">
         <Welcome />
         <LoginForm />
       </div>
